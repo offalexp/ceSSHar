27565e8e13aae8a6cab3d1af4ad24a81442b6385
juan.penalba@offal.com.ar <juan.penalba@offal.com.ar>
2019-04-26 10:31:55 -0300

    se actualizo .gitignor y levanto monitoreo.py

---
+++ monitoreo.py
@@ -0,0 +1,22 @@
+try:
+    import logging
+    from claseCrassh import clsCrassh
+    import logging.handlers
+    import itertools
+    import configparser
+    import os, sys
+    from bottle import route, run
+except Exception as e:
+    print("No se pudo importar un modulo requerido: " + str(e))
+
+def traeestado(swEelegido, swCmdElegido):
+    config = configparser.ConfigParser()
+    config.__username = adminOffal
+    config.__password = L2las1sc)    
+    nombre = self.conexion.connect(, config.__username, config.__password)
+    config.nombreEquipo = nombre
+
+
+
+
+
